2aecd9152bc21dec0e3b99cce67c436ff763d173
StratocasterDon <ricuxaba@binka.me>
2017-02-04 16:33:16 +0300

    Задание №5

--- Polaris/Polaris/Polaris.py
+++ Polaris/Polaris/Polaris.py
@@ -45,5 +45,11 @@ def z4():
     for i in userstr:
         print(i)
 
+def z5():
+    userstr = (str(input('¬ведите свою бесполезную строку:\n'))).split(' ')
+    for i in range(len(userstr)):
+        if ((userstr[i])[0]).isupper():
+            userstr[i] = userstr[i].upper()
+    print(' '.join(userstr))
 
-z4()
+z5()
