160d05242a2fb086b2b4365a9968d8683f136c7d
StratocasterDon <ricuxaba@binka.me>
2017-02-04 15:04:26 +0300

    Задание №3 + косметика

--- Polaris/Polaris/Polaris.py
+++ Polaris/Polaris/Polaris.py
@@ -1,26 +1,32 @@
 # -*- coding: cp1251 -*-
 
 #Задание №1:
-try:
-    userinput = float(input('Введите своё бесполезное число в вещественном виде: '))
-    if userinput < 0:
-        raise ValueError('Число отрицательное')
-    print('{} руб. {} коп.'.format(str(userinput).split('.')[0], str(userinput).split('.')[1])) #Костыль-мастер
-except ValueError as ex:
-    print('An error occured: ValueError')
-except:
-    print('Ваш ввод слишком криворукий для этого скрипта')
+def z1():
+    try:
+        userinput = float(input('Введите своё бесполезное число в вещественном виде: '))
+        if userinput < 0:
+            raise ValueError('Число отрицательное')
+        print('{} руб. {} коп.'.format(str(userinput).split('.')[0], str(userinput).split('.')[1])) #Костыль-мастер
+    except ValueError as ex:
+        print('An error occured: ValueError')
+    except:
+        print('Ваш ввод слишком криворукий для этого скрипта')
 
 #Задание №2
-userinput = int(input('Введите некое число: '))
-rangelist = range(userinput)
-isup = True
-temp = -1
-for i in rangelist:
-    if i <= temp:
-        rangelist = False
-        break
-    else:
-        temp = i
-print(isup)
+def z2():
+    rangelist = range(int(input('Введите некое число: ')))
+    isup = True
+    temp = -1
+    for i in rangelist:
+        if i <= temp:
+            rangelist = False
+            break
+        else:
+            temp = i
+    print(isup)
+
+#Задание №3
+def z3():
+    userstr = str(input('Введите номер вашей кредитки, если хотите повысить нам настроение: '))
+    print('{} **** **** {}'.format(userstr[4:6], userstr[13:16]))
 
