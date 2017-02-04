2b954c13139da13bd2be1cbecfa01e01b2b25978
StratocasterDon <ricuxaba@binka.me>
2017-02-04 14:53:21 +0300

    Первое задание ЛР1

--- Polaris/Polaris/Polaris.py
+++ Polaris/Polaris/Polaris.py
@@ -1,2 +1,13 @@
 # -*- coding: cp1251 -*-
 
+#Задание №1:
+try:
+    userinput = float(input('Введите своё бесполезное число в вещественном виде: '))
+    if userinput < 0:
+        raise ValueError('Число отрицательное')
+    print('{} руб. {} коп.'.format(str(userinput).split('.')[0], str(userinput).split('.')[1])) #Костыль-мастер
+except ValueError as ex:
+    print('An error occured: ValueError')
+except:
+    print('Ваш ввод слишком криворукий для этого скрипта')
+
