f2373358a6fb3b6de35a44d3b77d2c10dda29d2a
StratocasterDon <ricuxaba@binka.me>
2017-02-05 17:56:21 +0300

    ЗАдание №8

--- Polaris/Polaris/Polaris.py
+++ Polaris/Polaris/Polaris.py
@@ -73,4 +73,20 @@ def z7():
     for i in templist:
         print(i)
 
-z7()
+def z8():
+    from random import randint
+    from math import log
+    random_numbers = []
+    randmaxnumber = int(input('Введите размер массива\n'))
+    for i in range(1<<int(round(log(randmaxnumber,2)))):
+        if i <= randmaxnumber:
+            random_numbers.append(randint(1, 10000))
+        else:
+            random_numbers.append(0)
+    print('Случайные числа')
+    print(' '.join(str(random_numbers)))
+    print('Количество элементов в списке {}'.format(len(random_numbers)))
+    print('Отдельное случайное число - {}'.format(randint(1, 10000)))
+
+
+z8()
