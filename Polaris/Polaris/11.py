8e87b9f5a2f911478f34543746bb81beb53d4ea7
StratocasterDon <ricuxaba@binka.me>
2017-02-12 18:36:30 +0300

    Одинадцатое задание

--- Polaris/Polaris/11.py
+++ Polaris/Polaris/11.py
@@ -2,9 +2,9 @@
 
 def frange(start, stop, step=0.1):
     i = start
-    while i < stop:
+    while i < stop-step*2:
         i += step
         yield round(i, len(str(step))-2)
 
-for i in frange(1, 5, 0.25):
+for i in frange(1, 5):
     print(i)
