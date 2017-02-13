e370d6890923e481b90211f518d17462469b0e0f
StratocasterDon <ricuxaba@binka.me>
2017-02-13 12:57:44 +0300

    Задание 11 фиксед

--- Polaris/Polaris/11.py
+++ Polaris/Polaris/11.py
@@ -1,7 +1,7 @@
 # -*- coding: cp1251 -*-
 
 def frange(start, stop, step=0.1):
-    i = start
+    i = start-step
     while i < stop-step*2:
         i += step
         yield round(i, len(str(step))-2)
