c33221070157c2fc960b83cb7ed7246f12b3aaf9
StratocasterDon <ricuxaba@binka.me>
2017-02-24 16:49:33 +0300

    Backups

--- Polaris/Polaris/3.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: cp1251 -*-
-
-userstr = str(input('Введите номер вашей кредитки, если хотите повысить нам настроение: '))
-if not userstr.isdigit():
-    print('Номер кредитной карты может состоять только из цифр')
-elif len(userstr) < 16 or len(userstr) > 16:
-    print('Ошибка длины номера карты: допустимо только 16 цифр')
-else:
-    print('{} **** **** {}'.format(userstr[0:4], userstr[12:16]))
--- Polaris/Polaris/5.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: cp1251 -*-
-
-userstr = (str(input('¬ведите свою бесполезную строку:\n'))).split(' ')
-for i in range(len(userstr)):
-    if ((userstr[i])[0]).isupper():
-        userstr[i] = userstr[i].upper()
-print(' '.join(userstr))
