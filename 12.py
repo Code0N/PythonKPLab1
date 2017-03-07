be16e3e53acf0abe22cf5eb551da227bd075bff9
StratocasterDon <ricuxaba@binka.me>
2017-03-07 17:34:11 +0300

    Задание 12 rewrited

--- 12.py
+++ 12.py
@@ -1,16 +1,13 @@
-from math import ceil
+def get_frames(signal,size,overlap):   
+    step = size * overlap
+    print (step)
+    i = 0
+    while i<len(signal):
+        yield (signal[i:i+size])
+        i = i + int(step)
 
-def get_frames(signal, size=1024, overlap=0.5):
-	_i = -ceil(overlap) #begin
-	j = (size-1)-overlap #end
-	for i in range(len(signal)):
-		_i += ceil(overlap)
-		j += ceil(overlap)
-		temp = signal[_i:j]
-		if temp != []:
-			yield temp
-		
 
-signal = [i for i in range(1000)]
-for i in get_frames(signal, 16, 5):
+x = [i for i in range(1024)]
+
+for i in get_frames(x, 4, 0.5):
 	print(i)
